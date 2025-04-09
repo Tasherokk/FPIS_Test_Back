3b40533a2a3cc7c74e79c7e71b1f2913dd3f9dec
Olzhas <olzhas.taukent@gmail.com>
2025-04-09 20:20:46 +0600

    modified settings

--- ubt_project_back/ubt_platform/ubt_platform/settings.py
+++ ubt_project_back/ubt_platform/ubt_platform/settings.py
@@ -22,9 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=reh$&efify35*fm261ttphraklv&q#ts5)028#iy07mjz*6h='
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
